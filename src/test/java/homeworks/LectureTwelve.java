018779ed03799ceb96a0842d088e4efbf6b303fa
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-22 14:00:29 +0200

    lecture 12 homework update

--- src/test/java/homeworks/LectureTwelve.java
+++ src/test/java/homeworks/LectureTwelve.java
@@ -10,23 +10,34 @@ import java.time.Duration;
 public class LectureTwelve {
 
     @Test
-    private static void firstAutomatedTest() {
+    public static void login() {
 
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.get("https://www.esky.bg/");
 
         WebElement cookie = driver.findElement(By.xpath("//*[@id=\"qc-cmp2-ui\"]/div[2]/div/button[2]/span"));
-
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         cookie.click();
 
         WebElement login = driver.findElement(By.xpath("//*[@class=\"account-title\"]"));
-
         login.click();
 
+        WebElement loginType = driver.findElement(By.xpath("//ul/li[3]/span"));
+        loginType.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement username = driver.findElement(By.xpath("//input[@type=\"email\"]"));
+        username.click();
+        username.clear();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.close();
+
+
     }
+
 
 }
