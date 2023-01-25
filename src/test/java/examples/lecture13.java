944d4935e7caa631486d03a2cf81f9b3957e50ab
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-25 19:20:04 +0200

    lecture 13 exercise

---
+++ src/test/java/examples/lecture13.java
@@ -0,0 +1,61 @@
+package examples;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class lecture13 {
+
+    @Test
+    public void testLogin(){
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        WebElement loginButton = driver.findElement(By.id("nav-link-login"));
+        loginButton.click();
+
+        /*String expectedLoginURL = "http://training.skillo-bg.com:4300/users/login";
+        String actualLoginURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualLoginURL, expectedLoginURL);*/
+
+        WebElement signInTitle = driver.findElement(By.xpath("//p[text()='Sign in']"));
+        Assert.assertTrue(signInTitle.isDisplayed());
+
+        WebElement username = driver.findElement(By.id("defaultLoginFormUsername"));
+        username.sendKeys("earnaudova@gmail.com");
+
+        WebElement password = driver.findElement(By.id("defaultLoginFormPassword"));
+        password.sendKeys("test123");
+
+        WebElement signInButton = driver.findElement(By.id("sign-in-button"));
+        Assert.assertTrue(signInButton.isEnabled());
+        signInButton.click();
+
+        WebElement profileLink = driver.findElement(By.id("nav-link-profile"));
+        Assert.assertTrue(profileLink.isDisplayed(), "The profile link is not displayed!");
+
+        profileLink.click();
+
+        /*String actualProfilePageUrl = driver.getCurrentUrl();
+        String expectedProfilePage = "http://training.skillo-bg.com:4300/users/3905";
+        Assert.assertEquals(actualProfilePageUrl, expectedProfilePage, "The profile page url is incorrect!");*/
+
+        WebElement userNameElement = driver.findElement(By.tagName("h2"));
+        String actualUserName = userNameElement.getText();
+        String expectedUserName = "earnaudova";
+        Assert.assertEquals(actualUserName, expectedUserName, "The user name is incorrect!");
+
+
+    }
+
+}
